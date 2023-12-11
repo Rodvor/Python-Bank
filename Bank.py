fcd54c6c616490d685dae603f8fa6bbb5f19e542
Hugo Minkkinen <mr.rodvor@gmail.com>
2023-12-11 11:52:17 +0200

    Added Features
    + Admin settings
    + Transactions & Change password
    + Automatic Admin & Bank account creation

--- Bank.py
+++ Bank.py
@@ -11,11 +11,15 @@ def loginView():
         print('Python Bank - Login')
         print('-------------------')
         name = input('Name: ')
+
+        if name.replace(' ','') == '': break
+
         password = input('Password: ')
 
         if loginRequest(name, password):
 
-            accountView(name)
+            if name.lower() == "admin": adminView()
+            else: accountView(name)
         
         else:
 
@@ -31,13 +35,86 @@ def accountView(name):
 
     while not leaveView:
 
-        option = drawScreen(f'Python Bank - Account: {account.name}\nBalance: {account.balance} €', ['Transfer Money','Change Password','Exit'])
+        option = drawScreen(f'Python Bank - Account: {account.name}\nBalance: {account.balance:,} €', ['Transfer Money','Change Password','Exit'])
 
         clear()
-
         
         if option == 1:
-            pass 
+            
+            print('Python Bank - Transfer Money')
+            print('----------------------------')
+            print(f'Balance: {account.balance:,} €')
+            print('----------------------------')
+            receiver = input('Receiver: ')
+            amount = input('Amount: ')
+
+            result = account.transfer(receiver, amount)
+
+            clear()
+            print(result)
+            sleep(1)
+
+
+        elif option == 2:
+
+            print('Python Bank - Change Password')
+            print('-----------------------------')
+            oldPass = input('Old Password: ')
+            newPass = input('New Password: ')
+
+            clear()
+
+            if oldPass == account.password:
+
+                account.password = newPass
+                account.save()
+
+                print('Password changed!')
+            
+            else:
+
+                print('Incorrect Password!')
+            
+            sleep(1)
+
+
+        elif option == 3:
+            leaveView = True
+
+        else:
+
+            print("Invalid option!")
+            sleep(1)
+
+def adminView():
+
+    leaveView = False
+
+    while not leaveView:
+
+        option = drawScreen(f'Python Bank - Admin Settings', ['Create Account','Delete Account','Exit'])
+        
+        clear()
+
+        if option == 1:
+            
+            print('Python Bank - Create Account')
+            print('----------------------------')
+            name = input('Name: ')
+            password = input('Password: ')
+
+            clear()
+
+            if Account(name).create(password):
+
+                print('Account created!')
+
+            else:
+
+                print('Account already exists!')
+            
+            sleep(1)
+
 
         elif option == 2:
             pass
@@ -49,7 +126,6 @@ def accountView(name):
 
             print("Invalid option!")
             sleep(1)
-
 
 
 class Account:
@@ -72,7 +148,9 @@ class Account:
 
         if not self.exists:
 
-            self.balance = 0
+            if self.name == 'Bank': self.balance = 1_000_000_000
+            else: self.balance = 0
+
             self.password = password
 
             self.save()
@@ -80,6 +158,33 @@ class Account:
             return True
 
         return False
+    
+    def transfer(self, receiver, amount):
+        
+        try: amount = int(amount)
+        except: return 'Amount must be numerical!'
+
+        receiver = Account(receiver)
+
+        if not receiver.exists:
+
+            return 'Receiver account does not exist!'
+
+        if amount > self.balance:
+
+            return 'Insufficient funds!'
+
+        if amount < 0:
+
+            return 'Amount must be positive!'
+        
+        self.balance -= amount
+        receiver.balance += amount
+
+        self.save()
+        receiver.save()
+
+        return f'Successfully transferred {amount}€ to {receiver.name}!'
     
     def save(self):
 
@@ -143,7 +248,17 @@ def writeFile(path, text):
     with open(path, mode='w') as file:
         file.write(text)
 
-if not os.path.exists('Accounts'):
+
+
+# Program Start
+
+if not os.path.exists('Accounts'): #Creates Accounts folder if it does not exist
     os.mkdir('Accounts')
+
+if not os.path.exists('Accounts/Admin'): #Creates Admin account with password 0000 if it does not exist
+    Account('Admin').create('0000')
+
+if not os.path.exists('Accounts/Bank'): #Creates Bank account with password 0000 if it does not exist
+    Account('Bank').create('0000')
 
 loginView()
